21f0f8aa4e7e1ec7942e2a30366de82e89af51b6
andre <andreevicctor@live.com>
2022-09-03 14:13:44 -0300

    Enviando arquivos do primeiro curso

---
+++ database/migrations/2022_09_03_123027_create_series_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 128); // campo do tipo string e tamanho 128 caracteres
+            $table->timestamps(); // campos de inseridos e atualizados
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+};
