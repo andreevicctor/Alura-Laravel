95dd5de457b9a10be481680164e97661bdbcc321
andre <andreevicctor@live.com>
2022-09-12 20:38:22 -0300

    Adicionando transacoes

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\SeriesFormRequest;
 use App\Models\Episode;
 use App\Models\Season;
 use App\Models\Series;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
@@ -23,26 +24,30 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = Series::create($request->all()); // o ::create já retorna a model criada
-        $seasons = [];
-        for ($i = 1; $i <= $request->seasonsQty; $i++) {
-            $seasons[] = [
-                'series_id' => $serie->id,
-                'number' => $i
-            ];
-        }
-        Season::insert($seasons);
-
-        $episodes = [];
-        foreach($serie->seasons as $season) {
-            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                $episodes[] = [
-                    'season_id' => $season->id,
-                    'number' => $j
+        $serie = DB::transaction(function () use ($request) {
+            $serie = Series::create($request->all()); // o ::create já retorna a model criada
+            $seasons = [];
+            for ($i = 1; $i <= $request->seasonsQty; $i++) {
+                $seasons[] = [
+                    'series_id' => $serie->id,
+                    'number' => $i
                 ];
             }
-        }
-        Episode::insert($episodes);
+            Season::insert($seasons);
+    
+            $episodes = [];
+            foreach($serie->seasons as $season) {
+                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                    $episodes[] = [
+                        'season_id' => $season->id,
+                        'number' => $j
+                    ];
+                }
+            }
+            Episode::insert($episodes);
+
+            return $serie;
+        });
         
 //      Series::create($request->only(['nome'])); traz somente os campos do only(['campo1','campo2'])
 //      Series::create($request->except(['_token'])); traz todos os campos exceto os informados no array
